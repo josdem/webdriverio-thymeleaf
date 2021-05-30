19b266102374a7ca562c0a15fa8db409c3701b61
josdem <joseluis.delacruz@gmail.com>
2021-05-30 15:35:40 -0400

    #1 Adding search locations step

--- test/pageobjects/order.page.js
+++ test/pageobjects/order.page.js
@@ -5,12 +5,13 @@ class OrderPage extends Page {
     get zipCode() { return $('input[id="Postal_Code_Sep"]') }
     get city() { return $('input[id="City_Sep"]') }
     get state() { return $('select[id="Region_Sep"] option[value="MI"]') }
+    get searchLocationsButton() { return $('button[type="submit"]') }
     
     async selectZipCodeCityAndState() {
         (await this.zipCode).setValue(properties.zipCode);
         (await this.city).setValue(properties.city);
         (await this.state).click();
-        await browser.pause(10000);
+        (await this.searchLocationsButton).click();
     }
 
     open() {
