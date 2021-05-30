4b1253d7ada8c807da1c6682f770dba77d502d12
josdem <joseluis.delacruz@gmail.com>
2021-05-30 15:27:32 -0400

    #1 Adding state select step

--- test/pageobjects/order.page.js
+++ test/pageobjects/order.page.js
@@ -4,10 +4,13 @@ let properties = require('../config/properties');
 class OrderPage extends Page {
     get zipCode() { return $('input[id="Postal_Code_Sep"]') }
     get city() { return $('input[id="City_Sep"]') }
+    get state() { return $('select[id="Region_Sep"] option[value="MI"]') }
     
     async selectZipCodeCityAndState() {
         (await this.zipCode).setValue(properties.zipCode);
         (await this.city).setValue(properties.city);
+        (await this.state).click();
+        await browser.pause(10000);
     }
 
     open() {
