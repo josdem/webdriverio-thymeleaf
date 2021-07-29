9474b6e005a73ececd52d9d97735ac9208308157
josdem <joseluis.delacruz@gmail.com>
2021-07-29 16:48:29 -0400

    #6 Deleting semicolon from checkout page

--- test/pageobjects/checkout.page.js
+++ test/pageobjects/checkout.page.js
@@ -1,14 +1,14 @@
-const Page = require('./page');
-let properties = require('../config/properties');
+const Page = require('./page')
+let properties = require('../config/properties')
 
 class CheckoutPage extends Page {
     get itemFromCart() { return $('div[class="media-cart"]') }
 
     async validateItem() {
-        const itemConst = await this.itemFromCart;
-        await expect(itemConst).toBeExisting(); 
+        const itemConst = await this.itemFromCart
+        await expect(itemConst).toBeExisting()
     }
 
 }
 
-module.exports = new CheckoutPage();
+module.exports = new CheckoutPage()
