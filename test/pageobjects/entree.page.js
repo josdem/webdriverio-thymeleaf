8dc5f90562bfeaf7a724f3156ec46c636b5b1203
josdem <joseluis.delacruz@gmail.com>
2021-07-29 16:47:50 -0400

    #6 Deleting semicolon from entree page

--- test/pageobjects/entree.page.js
+++ test/pageobjects/entree.page.js
@@ -1,15 +1,15 @@
-const Page = require('./page');
-let properties = require('../config/properties');
+const Page = require('./page')
+let properties = require('../config/properties')
 
 class EntreePage extends Page {
     get pizza() { return $('div[class="media-pizza"]') }
 
     async selectPizza() {
-        const pizzaConst = await this.pizza;
-        expect(pizzaConst).toBeExisting();
-        await pizzaConst.click();
+        const pizzaConst = await this.pizza
+        expect(pizzaConst).toBeExisting()
+        await pizzaConst.click()
     }
 
 }
 
-module.exports = new EntreePage();
+module.exports = new EntreePage()
