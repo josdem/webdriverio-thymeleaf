6203bceae08cb8a7a8e0b04d009bf00446f780f2
josdem <joseluis.delacruz@gmail.com>
2021-07-29 16:46:50 -0400

    #6 Deleting semicolon from search page

--- test/pageobjects/search.page.js
+++ test/pageobjects/search.page.js
@@ -1,15 +1,15 @@
-const Page = require('./page');
-let properties = require('../config/properties');
+const Page = require('./page')
+let properties = require('../config/properties')
 
 class SearchPage extends Page {
     get driveUpCarryout() { return $('a[data-guid="drive-up-carryout"]') }
 
     async selectDriveUpCarryout() {
-        const driveUpCarryoutConst = await this.driveUpCarryout;
-        await expect(driveUpCarryoutConst).toBeExisting(); 
-        driveUpCarryoutConst.click();
+        const driveUpCarryoutConst = await this.driveUpCarryout
+        await expect(driveUpCarryoutConst).toBeExisting()
+        driveUpCarryoutConst.click()
     }
 
 }
 
-module.exports = new SearchPage();
+module.exports = new SearchPage()
