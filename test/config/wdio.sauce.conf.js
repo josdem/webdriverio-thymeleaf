b33a527e71196af187677765f8d73121aa8f5fed
josdem <joseluis.delacruz@gmail.com>
2021-09-04 12:14:12 -0500

    #10 Adding local and Saucelabs configurations

---
+++ test/config/wdio.sauce.conf.js
@@ -0,0 +1,19 @@
+const { config } = require("../../wdio.conf")
+
+config.user = process.env.SAUCE_USERNAME
+config.key = process.env.SAUCE_ACCESS_KEY
+
+config.services = config.services.concat('sauce')
+
+config.capabilities = [
+  {
+    browserName: "chrome",
+    browserVersion: "92",
+    platformName: "Windows 10",
+    "sauce:options": {
+      screenResolution: "1920x1080",
+    },
+  },
+]
+
+exports.config = config
