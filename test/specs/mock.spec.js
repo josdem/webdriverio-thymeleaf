5dda391deaaec4a6560d3a08ee12ca6602d3d069
josdem <joseluis.delacruz@gmail.com>
2021-10-13 17:24:49 -0400

    #17 Adding mock spec

---
+++ test/specs/mock.spec.js
@@ -0,0 +1,12 @@
+const properties = require(`../properties/${process.env.NODE_ENV}.properties`)
+const OrderPage = require("../pageobjects/order.page")
+const REDIRECTION = "https://webdriver.io"
+
+describe("Mocking the response", () => {
+  it("should change localhost to webdriverio", async () => {
+    const pageMock = await browser.mock(properties.url)
+    pageMock.respond(REDIRECTION)
+    await OrderPage.open()
+    console.log(await browser.getTitle())
+  })
+})
