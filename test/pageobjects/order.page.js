3d29a7b41510ed2d7e1030ef0a960a03df6ca9bc
josdem <joseluis.delacruz@gmail.com>
2021-10-22 08:25:27 -0400

    #18 Fixing selector names

--- test/pageobjects/order.page.js
+++ test/pageobjects/order.page.js
@@ -2,27 +2,27 @@ const Page = require("./page")
 const properties = require(`../properties/${process.env.NODE_ENV}.properties`)
 
 class OrderPage extends Page {
-  get zipCode() {
-    return $('input[id="Postal_Code_Sep"]')
+  get zipCodeInput() {
+    return $('[id="Postal_Code_Sep"]')
   }
-  get city() {
-    return $('input[id="City_Sep"]')
+  get cityInput() {
+    return $('[id="City_Sep"]')
   }
-  get state() {
-    return $('select[id="Region_Sep"] option[value="MI"]')
+  get stateSelector() {
+    return $('[id="Region_Sep"] [value="MI"]')
   }
   get searchLocationsButton() {
-    return $('button[type="submit"]')
+    return $('[type="submit"]')
   }
 
   async selectZipCodeCityAndState() {
-    const zipCodeConst = await this.zipCode
+    const zipCodeConst = await this.zipCodeInput
     await expect(zipCodeConst).toBeExisting()
     zipCodeConst.setValue(properties.zipCode)
-    const cityConst = await this.city
+    const cityConst = await this.cityInput
     await expect(cityConst).toBeExisting()
     cityConst.setValue(properties.city)
-    const stateConst = await this.state
+    const stateConst = await this.stateSelector
     await expect(stateConst).toBeExisting()
     stateConst.click()
     const searchLocationsButtonConst = await this.searchLocationsButton
